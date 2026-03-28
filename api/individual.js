806af890aa046fd33684e7798a6583eadc030bba
lightdrinker <mnemosia.k@gmail.com>
2026-03-28 09:29:28 +0900

    fix: update individual API endpoint and response parsing

--- api/individual.js
+++ api/individual.js
@@ -1,14 +1,14 @@
 export default async function handler(req, res) {
   const API_KEY = process.env.VITE_API_KEY
-  const { pageNo = 1, numOfRows = 100 } = req.query
+  const { startIdx = 1, endIdx = 100 } = req.query
 
   try {
-    const url = `https://apis.data.go.kr/1471000/HlthFncFoodInfoService02/getHlthFncFoodIndvdlRcognInfo02?serviceKey=${API_KEY}&pageNo=${pageNo}&numOfRows=${numOfRows}&type=json`
+    const url = `http://openapi.foodsafetykorea.go.kr/api/${API_KEY}/I-0050/json/${startIdx}/${endIdx}`
     const response = await fetch(url)
     const data = await response.json()
 
     res.setHeader('Access-Control-Allow-Origin', '*')
-    res.status(200).json(data)
+    res.status(200).json(data['I-0050'])
   } catch (e) {
     res.status(500).json({ error: e.message })
   }
